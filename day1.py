208fd9bbbf7f7ab376624d6710401e44b9f49263
bikalp-java <bikalpa.chaudhary@yenyasoft.com>
2026-05-31 08:53:37 +0545

    day1: python AL ML workshop

---
+++ day1.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+
+name = "Bikalpa chaudhary"
+dob = "2026/02/02"
+
+print(f"Hello, {name}. Your DOB is: {dob}")
+
+# swap values in pythonq
+a, b = 12, 13
+print("Before swapping: a =", a, "b =", b)
+a, b = b, a
+print("After swapping: a =", a, "b =", b)
